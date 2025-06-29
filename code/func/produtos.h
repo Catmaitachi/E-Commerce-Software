f46708da5919e348d30d9fdccd9efcd07986e606
LinusPfour <luizpaulo.delgado@yahoo.com.br>
2025-06-29 04:14:33 -0300

    feat: função obter e pesquisar produto

--- code/func/produtos.h
+++ code/func/produtos.h
@@ -48,4 +48,42 @@ void MenuProdutos(void) {
         default: MenuProdutos(); break;
     }
 }
+
+// Funções CRUD para obter o próximo código
+
+int ProximoProduto(void) {
+    FILE *arquivo = fopen(PRODUTOS, "r");
+    if (arquivo == NULL) {
+        return 1;
+    }
+    produto p;
+    int id = 0;
+    while (fscanf(arquivo, "%d,%49[^,],%d,%f\n", &p.codigo, p.nome, &p.estoque, &p.preco) == 4) {
+        if (p.codigo > id) {
+            id = p.codigo;
+        }
+    }
+    fclose(arquivo);
+    return id + 1;
+}
+
+// Função para pesquisar um produto pelo código
+
+int PesquisarProduto(int codigo, produto *res) {
+    FILE *arquivo = fopen(PRODUTOS, "r");
+    if (arquivo == NULL) {
+        return 0;
+    }
+    produto p;
+    while (fscanf(arquivo, "%d,%49[^,],%d,%f\n", &p.codigo, p.nome, &p.estoque, &p.preco) == 4) {
+        if (p.codigo == codigo) {
+            *res = p;
+            fclose(arquivo);
+            return 1;
+        }
+    }
+    fclose(arquivo);
+    return 0;
+}
+
 #endif
